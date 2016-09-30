e4d536da71f7c131913552d6674da0d66bd2b442
Russell Schmidt <russell.schmidt@me.com>
2016-09-30 15:53:52 -0700

    complete regex. mind blown.

--- assignment/regex.rb
+++ assignment/regex.rb
@@ -4,6 +4,9 @@ def convert_to_camel_case(str)
     # need to delete all underscores
     
     str.gsub!(/(^[a-z])/) { $1.upcase }
-    str.split("_").map {|x| x.capitalize}.join
+    str.gsub!(/(?<=_)[a-z]/){|x| x.capitalize}
+    str.gsub!(/[_]/){|x| ''}
+    # A different non-regex way to do this, replacing last two lines above 
+    # str.split("_").map {|x| x.capitalize}.join
 
 end
