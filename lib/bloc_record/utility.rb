bc9a8ad4d9360bba0ff2408ecbf7e0fe71f23788
Russell Schmidt <russell.schmidt@me.com>
2016-10-04 10:05:29 -0700

    fix missing square bracket on underscore()

--- lib/bloc_record/utility.rb
+++ lib/bloc_record/utility.rb
@@ -5,7 +5,7 @@ module BlocRecord
         def underscore(camel_cased_word)
             string = camel_cased_word.gsub(/::/, '/')
             string.gsub!(/([A-Z]+)([A-Z][a-z])/,'\1_\2')
-            string.gsub!(/([a-z\d])([A-Z)/,'\1_\2')
+            string.gsub!(/([a-z\d])([A-Z])/,'\1_\2')
             string.tr!("-","_")
             string.downcase
         end
