98b6fce97d917e0bdec0d38d910eb331dff02b8a
Russell Schmidt <russell.schmidt@me.com>
2016-10-04 10:22:25 -0700

    add find_by, find, find_one to selection.rb

--- lib/bloc_record/selection.rb
+++ lib/bloc_record/selection.rb
@@ -1,13 +1,46 @@
 require 'sqlite3'
 
 module Selection
-    def find_one(id)
-        row = connection.get_first_row <<-SQL
-            SELECT #{columns.join ","} FROM #{table}
-            WHERE id = #{id};
-        SQL
+  def find(*ids)
+  	if ids.length == 1
+  		find_one(ids.first)
+  	else
+  		rows = connection.execute <<-SQL
+  			SELECT #{columns.join ","} FROM #{table}
+  			WHERE id IN (#{ids.join(",")});
+  		SQL
 
-        data = Hash[columns.zip(row)]
-        new(data)
-    end
+  		rows_to_array(rows)
+  	end
+  end
+
+
+  def find_one(id)
+      row = connection.get_first_row <<-SQL
+          SELECT #{columns.join ","} FROM #{table}
+          WHERE id = #{id};
+      SQL
+
+      init_object_from_row(row)
+  end
+
+
+  def find_by(attribute, value)
+  	row = connection.get_first_row <<-SQL
+  		SELECT #{columns.join ","} FROM #{table}
+  		WHERE #{attribute} = #{BlocRecord::Utility.sql_strings(value)};
+  	SQL
+
+  	init_object_from_row(row)
+  end
+
+
+  private
+  def init_object_from_row(row)
+  	if row
+	  	data = Hash[columns.zip(row)]
+		  new(data)
+		end
+  end
+
 end
